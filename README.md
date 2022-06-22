b218e0c82bc679158fb672218a3f75881857b209
DivadNojnarg <dgranjon@ymail.com>
2022-06-22 16:06:05 +0200

    adding more examples

--- README.md
+++ README.md
@@ -3,12 +3,22 @@
 
 <!-- badges: start -->
 [![R-CMD-check](https://github.com/RinteRface/shinyHeatmap/workflows/R-CMD-check/badge.svg)](https://github.com/RinteRface/shinyHeatmap/actions)
+[![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
 <!-- badges: end -->
 
-The goal of shinyHeatmap is to ease Shiny app usage tracking by providing visual heatmap of
-app usage across many user sessions.
+The goal of `{shinyHeatmap}` is to provide a __free__ and __local__ alternative to more advanced user tracking platform such as [Hotjar](https://www.google.com/search?q=hotjar&oq=hotjar&aqs=chrome.0.69i59j0i512j69i60l6.1063j0j7&sourceid=chrome&ie=UTF-8).
 
-## Screenshots
+`{shinyHeatmap}` generates beautiful and persistent visual heatmaps, representing the app usage across many user sessions. If you ever wondered:
+
+- Is the left action button used?
+- Did people notice the new tab?
+- Is the top left checkbox still useful?
+
+You should give it a try.
+
+If you're concerned about data privacy, `{shinyHeatmap}` only records x and y clicks coordinates on the window.
+
+## Examples
 
 ### {shiny}
 <img src="man/figures/shinyHeatmap-basic.png">
@@ -19,6 +29,9 @@ app usage across many user sessions.
 ### {shinydashboard}
 <img src="man/figures/shinyHeatmap-shinydashboard.png">
 
+### {shiny.fluent}
+<img src="man/figures/shinyHeatmap-shinyFluent.png">
+
 ## Installation
 
 You can install the development version of `{shinyHeatmap}` from [GitHub](https://github.com/) with:
@@ -28,17 +41,22 @@ You can install the development version of `{shinyHeatmap}` from [GitHub](https:
 devtools::install_github("RinteRface/shinyHeatmap")
 ```
 
-## Example
+## Getting started
 
 ### How to use it
 
 - The app must have a `www` folder since 
 heatmap data are stored in `www/heatmap-data.json` by default.
-- In `ui.R`, wrap the UI inside `heatmap_container()`. This setup the canvas
+- In `ui.R`, wrap the UI inside `heatmap_container()`. This initializes the canvas
 to record the click coordinates.
 - In `server.R`, call `record_heatmap()`. Overall, this recovers the
 coordinates of each click on the JS side and store them in `www/heatmap-data.json`.
-This may be used later to preview the heatmap.
+This may be used later to preview the heatmap. With vanilla `{shiny}` templates
+like `fluidPage`, you don't need to change anything. However, with more complex templates,
+you can pass the target CSS selector of the heatmap container 
+with `record_heatmap(target = ".wrapper")`. If the app takes time to load, 
+a __timeout__ parameters is available. This could be the case when you rely on packages
+such as [{waiter}](https://github.com/JohnCoene/waiter).
 - Locally, you can add `download_heatmap()` to your app, which will read data stored
 in the JSON, generate the heatmap and save it as a png file:
 
@@ -53,7 +71,7 @@ observeEvent(input$get_heatmap, {
 ```
 
 In general, you don't want to use `download_heatmap()` on a deployed app since
-end users might not supposed to access usage data.
+end users might not be supposed to access and view usage data.
 
 Full code below:
 
@@ -107,3 +125,6 @@ server <- function(input, output, session) {
 shinyApp(ui = ui, server = server)
 ```
 
+## Acknowledgement
+
+`{shinyHeatmap}` is proudly powered by the excellent and free [heatmap.js](https://github.com/pa7/heatmap.js) library. Thanks [@pa7](https://github.com/pa7) for making this possible.
---
+++ inst/examples/shiny-fluent/app.R
@@ -0,0 +1,132 @@
+library(shiny)
+library(shiny.fluent)
+library(dplyr)
+library(glue)
+library(plotly)
+library(leaflet)
+library(shinyHeatmap)
+
+makeCard <- function(title, content, size = 12, style = "") {
+  div(
+    class = glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
+    style = style,
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "large", title, block = TRUE),
+      content
+    )
+  )
+}
+
+filters <- Stack(
+  tokens = list(childrenGap = 10),
+  Stack(
+    horizontal = TRUE,
+    tokens = list(childrenGap = 10),
+    DatePicker.shinyInput("fromDate", value = as.Date('2020/01/01'), label = "From date"),
+    DatePicker.shinyInput("toDate", value = as.Date('2020/12/31'), label = "To date")
+  ),
+  Label("Filter by sales reps", className = "my_class"),
+  NormalPeoplePicker.shinyInput(
+    "selectedPeople",
+    class = "my_class",
+    options = fluentPeople,
+    pickerSuggestionsProps = list(
+      suggestionsHeaderText = 'Matching people',
+      mostRecentlyUsedHeaderText = 'Sales reps',
+      noResultsFoundText = 'No results found',
+      showRemoveButtons = TRUE
+    )
+  ),
+  Slider.shinyInput("slider",
+                    value = 0, min = 0, max = 1000000, step = 100000,
+                    label = "Minimum amount",
+                    valueFormat = JS("function(x) { return '$' + x}"),
+                    snapToStep = TRUE
+  ),
+  Toggle.shinyInput("closedOnly", value = TRUE, label = "Include closed deals only?")
+)
+
+details_list_columns <- tibble(
+  fieldName = c("rep_name", "date", "deal_amount", "client_name", "city", "is_closed"),
+  name = c("Sales rep", "Close date", "Amount", "Client", "City", "Is closed?"),
+  key = fieldName)
+
+# ---- 05_ui
+ui <- heatmap_container(
+  fluentPage(
+    tags$style(".card { padding: 28px; margin-bottom: 28px; }"),
+    actionButton("get_heatmap", "Get heatmap"),
+    Stack(
+      tokens = list(childrenGap = 10), horizontal = TRUE,
+      makeCard("Filters", filters, size = 4, style = "max-height: 320px"),
+      makeCard("Deals count", plotlyOutput("plot"), size = 8, style = "max-height: 320px")
+    ),
+    uiOutput("analysis")
+  )
+)
+# ----
+
+server <- function(input, output, session) {
+  
+  record_heatmap(target = "body")
+  
+  observeEvent(input$get_heatmap, {
+    download_heatmap()
+  })
+  
+  filtered_deals <- reactive({
+    req(input$fromDate)
+    selectedPeople <- (
+      if (length(input$selectedPeople) > 0) input$selectedPeople
+      else fluentPeople$key
+    )
+    minClosedVal <- if (isTRUE(input$closedOnly)) 1 else 0
+    
+    filtered_deals <- fluentSalesDeals %>%
+      filter(
+        rep_id %in% selectedPeople,
+        date >= input$fromDate,
+        date <= input$toDate,
+        deal_amount >= input$slider,
+        is_closed >= minClosedVal
+      ) %>%
+      mutate(is_closed = ifelse(is_closed == 1, "Yes", "No"))
+  })
+  
+  # ---- 05_render
+  output$map <- renderLeaflet({
+    points <- cbind(filtered_deals()$LONGITUDE, filtered_deals()$LATITUDE)
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE)) %>%
+      addMarkers(data = points)
+  })
+  
+  output$plot <- renderPlotly({
+    p <- ggplot(filtered_deals(), aes(x = rep_name)) +
+      geom_bar(fill = unique(filtered_deals()$color)) +
+      ylab("Number of deals") +
+      xlab("Sales rep") +
+      theme_light()
+    ggplotly(p, height = 300)
+  })
+  # ----
+  
+  output$analysis <- renderUI({
+    items_list <- if(nrow(filtered_deals()) > 0){
+      DetailsList(items = filtered_deals(), columns = details_list_columns)
+    } else {
+      p("No matching transactions.")
+    }
+    
+    # ---- 05_outputs
+    Stack(
+      tokens = list(childrenGap = 10), horizontal = TRUE,
+      makeCard("Top results", div(style="max-height: 500px; overflow: auto", items_list)),
+      makeCard("Map", leafletOutput("map"))
+    )
+    # ----
+  })
+}
+
+shinyApp(ui, server)
